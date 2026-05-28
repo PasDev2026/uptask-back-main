f4dbb02d3248f45cab9338b971182eeac20abe37
ComputerScience-afk <computer.science@insalud.pe>
2026-05-28 15:06:49 -0500

    feat: add new areas

--- src/model/Empresa.ts
+++ src/model/Empresa.ts
@@ -9,7 +9,7 @@ const empresaSchema: Schema = new Schema({
         type: String,
         required: true,
         unique: true,
-        enum: ["jesus maria", "golf", "sjm", "hub"]
+        enum: ["jesus maria", "golf", "sjm", "hub", "operaciones", "talentos", "marketing", "finanzas", "contabilidad", "ti"]
     }
 })
 
--- src/utils/seed.ts
+++ src/utils/seed.ts
@@ -4,7 +4,7 @@ import Area from "../model/area"
 import { roleTypes } from "../model/role"
 import { areaTypes } from "../model/area"
 
-const SEDES = ["jesus maria", "golf", "sjm", "hub"]
+const SEDES = ["jesus maria", "golf", "sjm", "hub", "operaciones", "talentos", "marketing", "finanzas", "contabilidad", "ti"]
 
 export const seedEmpresas = async () => {
     try {
